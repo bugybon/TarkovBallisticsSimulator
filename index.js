8977a70f1660e4178b4caf1dd229f00467268532
Georgi Paleykov <georgipaleikov@gmail.com>
2025-01-08 15:36:34 +0200

    Now have communication between frontend and backend

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const path = require('path');
 
 const app = express();
 
+const cors = require('cors');
+app.use(cors());
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/pages'));
 app.use(express.static('node_modules'));
@@ -19,6 +22,26 @@ app.use(globalErrorHandler);
 app.get('/', (req, res) => {
   res.render('home');
 })
+
+const armorData = [
+  { name: "PACA Soft Armor", class: 2, material: "Aramid" },
+];
+
+app.get('/armor', (req, res) => {
+  const armorName = req.query.name;
+
+  if (!armorName) {
+      return res.status(400).json({ error: "Armor name is required" });
+  }
+
+  const armor = armorData.find(a => a.name.toLowerCase() === armorName.toLowerCase());
+
+  if (!armor) {
+      return res.status(404).json({ error: "Armor not found" });
+  }
+
+  res.json(armor);
+});
 
 app.listen(8080, function () {
   console.log('Server is listening on http//localhost:8080');
